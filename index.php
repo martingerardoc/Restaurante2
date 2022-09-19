6b4ca0c5c24b9ed8675776876f08ce3153bc295a
martingerardoc <martingerardoc56@gmail.com>
2022-09-19 14:59:08 -0300

    carga de datos

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EL TREVOL RESTAURANTE</title>
+	<link rel="stylesheet" href="./style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet"> 
+
+	<link rel="stylesheet" href="dos.css">
+	<link rel="stylesheet" href="tres.css">
+	<link rel="stylesheet" href="cuatro.css">
+
+</head>
+<body>
+	<h1>EL TREVOL RESTAURANTE</h1>
+	<a href="staff" class="boton dos"><span>Mesa1</span></a>
+			<a href="staff" class="boton dos"><span>Mesa2</span></a>
+			<a href="staff" class="boton dos"><span>Meseros</span></a>
+			<a href="admin" class="boton tres"><span>Administracion</span></a>
+			<a href="staff" class="boton cuatro"><span>Chef</span></a>
+</body>
+
+</html>
